000a03c00e8bc58cf822db1b1b985d9fc14a8ef5
Toti 犬の島 <57876954+ninjasan420@users.noreply.github.com>
2020-06-06 19:23:23 +0200

    fixed trap

--- commands/trap.js
+++ commands/trap.js
@@ -1,23 +1,26 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+module.exports = {
+        name: 'trap', // command name - wird nach prefix angeführt
+        aliases: ['schwulis'], //
 
-client.on('message', message => {
-    if (message.content === '§trap') loadCuties(message);
-});
+        const: Discord = require('discord.js'),
 
-function loadCuties() {
-    fetch('https://www.reddit.com/r/traps.json?limit=100&?sort=top&t=all')
-        .then(res => res.json())
-        .then(json => json.data.children.map(v => v.data.url))
-        .then(urls => postRandomCutie(urls));
-}
+        client: on('message', message => {
+            if (message.content === '§trap') loadCuties(message);
+        }),
 
-function postRandomCutie(urls) {
-    const randomURL = urls[Math.floor(Math.random() * urls.length) + 1];
-    const embed = new Discord.RichEmbed({
-        image: {
-            url: randomURL
+        function: loadCuties() {
+            fetch('https://www.reddit.com/r/traps.json?limit=100&?sort=top&t=all')
+                .then(res => res.json())
+                .then(json => json.data.children.map(v => v.data.url))
+                .then(urls => postRandomCutie(urls));
         }
-    });
-    message.channel.send(embed);
-}
+
+        function postRandomCutie(urls) {
+            const randomURL = urls[Math.floor(Math.random() * urls.length) + 1];
+            const embed = new Discord.RichEmbed({
+                image: {
+                    url: randomURL
+                }
+            });
+            message.channel.send(embed);
+        }
