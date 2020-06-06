01b21d5220da26edd198d300e89daa373340375f
Toti 犬の島 <57876954+ninjasan420@users.noreply.github.com>
2020-06-06 19:25:26 +0200

    update trap

--- commands/trap.js
+++ commands/trap.js
@@ -1,26 +1,27 @@
 module.exports = {
-        name: 'trap', // command name - wird nach prefix angeführt
-        aliases: ['schwulis'], //
+    name: 'trap', // command name - wird nach prefix angeführt
+    aliases: ['schwulis'], //
+}
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-        const: Discord = require('discord.js'),
+client.on('message', message => {
+    if (message.content === '§trap') loadCuties(message);
+});
 
-        client: on('message', message => {
-            if (message.content === '§trap') loadCuties(message);
-        }),
+function loadCuties() {
+    fetch('https://www.reddit.com/r/traps.json?limit=100&?sort=top&t=all')
+        .then(res => res.json())
+        .then(json => json.data.children.map(v => v.data.url))
+        .then(urls => postRandomCutie(urls));
+}
 
-        function: loadCuties() {
-            fetch('https://www.reddit.com/r/traps.json?limit=100&?sort=top&t=all')
-                .then(res => res.json())
-                .then(json => json.data.children.map(v => v.data.url))
-                .then(urls => postRandomCutie(urls));
+function postRandomCutie(urls) {
+    const randomURL = urls[Math.floor(Math.random() * urls.length) + 1];
+    const embed = new Discord.RichEmbed({
+        image: {
+            url: randomURL
         }
-
-        function postRandomCutie(urls) {
-            const randomURL = urls[Math.floor(Math.random() * urls.length) + 1];
-            const embed = new Discord.RichEmbed({
-                image: {
-                    url: randomURL
-                }
-            });
-            message.channel.send(embed);
-        }
+    });
+    message.channel.send(embed);
+}
