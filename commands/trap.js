b32c45f355fa48f497703379b2be452c25c51c76
Toti 犬の島 <57876954+ninjasan420@users.noreply.github.com>
2020-06-06 19:18:02 +0200

    traps added

---
+++ commands/trap.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('message', message => {
+    if (message.content === '§trap') loadCuties(message);
+});
+
+function loadCuties() {
+    fetch('https://www.reddit.com/r/traps.json?limit=100&?sort=top&t=all')
+        .then(res => res.json())
+        .then(json => json.data.children.map(v => v.data.url))
+        .then(urls => postRandomCutie(urls));
+}
+
+function postRandomCutie(urls) {
+    const randomURL = urls[Math.floor(Math.random() * urls.length) + 1];
+    const embed = new Discord.RichEmbed({
+        image: {
+            url: randomURL
+        }
+    });
+    message.channel.send(embed);
+}
