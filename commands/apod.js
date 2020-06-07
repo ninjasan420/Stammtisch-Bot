fb9f0d3a90a645401cc2b1b935a0a4066c926f27
Toti 犬の島 <57876954+ninjasan420@users.noreply.github.com>
2020-06-07 22:34:13 +0200

    fixed error

--- commands/apod.js
+++ commands/apod.js
@@ -1,42 +1,47 @@
 const Discord = require('discord.js');
 const axios = require('axios');
 
-module.exports = class spCommand extends Command {
-    constructor(client) {
-        super(client, {
-            name: 'apod',
-            alias: 'pott'
-        });
-    }
+module.exports = {
+    name: 'apod',
+    aliases: ['pott'],
 
-    async run(message) {
-
-        let getapod = async() => {
-            let response = await axios.get(
-                `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASAAPI}`
-            );
-            let apod = response.data;
-            return apod;
-        };
-        let apodvalue = await getapod();
-        const embed = new Discord.MessageEmbed()
-            .setTitle(`NASA Bild des Tages: ${apodvalue.title}`)
-            .setURL('https://apod.nasa.gov/apod/astropix.html')
-            .setAuthor('NASA', 'https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png', 'https://www.nasa.gov')
-            .setColor(0x5E00FF)
-            .setDescription(`${apodvalue.explanation}`)
-            .setImage(`${apodvalue.hdurl}`)
-            .setTimestamp()
-        const exampleEmbed = new Discord.MessageEmbed()
-            .setTitle(`NASA Bild des Tages: ${apodvalue.title} (Klicke fürs Video)`)
-            .setURL(`${apodvalue.url}`)
-            .setAuthor('NASA', 'https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png', 'https://www.nasa.gov')
-            .setColor(0x5E00FF)
-            .setDescription(`${apodvalue.explanation}`)
-            .setTimestamp()
-        if (`${apodvalue.media_type}` === 'video') {
-            message.channel.send(exampleEmbed)
+    execute(client, message, args) {
+        try {
+            getapod(message);
+        } catch (err) {
+            console.log(err);
         }
-        message.channel.send({ embed })
     }
+};
+
+async run(message) {
+
+    let getapod = async() => {
+        let response = await axios.get(
+            `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASAAPI}`
+        );
+        let apod = response.data;
+        return apod;
+    };
+    let apodvalue = await getapod();
+    const embed = new Discord.MessageEmbed()
+        .setTitle(`NASA Bild des Tages: ${apodvalue.title}`)
+        .setURL('https://apod.nasa.gov/apod/astropix.html')
+        .setAuthor('NASA', 'https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png', 'https://www.nasa.gov')
+        .setColor(0x5E00FF)
+        .setDescription(`${apodvalue.explanation}`)
+        .setImage(`${apodvalue.hdurl}`)
+        .setTimestamp()
+    const exampleEmbed = new Discord.MessageEmbed()
+        .setTitle(`NASA Bild des Tages: ${apodvalue.title} (Klicke fürs Video)`)
+        .setURL(`${apodvalue.url}`)
+        .setAuthor('NASA', 'https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png', 'https://www.nasa.gov')
+        .setColor(0x5E00FF)
+        .setDescription(`${apodvalue.explanation}`)
+        .setTimestamp()
+    if (`${apodvalue.media_type}` === 'video') {
+        message.channel.send(exampleEmbed)
+    }
+    message.channel.send({ embed })
+}
 };
