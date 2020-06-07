2179bb7cee983bb7849fdfc9f6f029d6cb9148e9
Toti 犬の島 <57876954+ninjasan420@users.noreply.github.com>
2020-06-07 23:58:09 +0200

    fixed app.js... again

--- app.js
+++ app.js
@@ -38,11 +38,13 @@ loadCommands('commands');
 // client ready event
 client.on('ready', () => {
     console.log('Bot is ready...');
+
+    client.user.setActivity(config.bot_settings.bot_status);
+
     client.channels.get(`718459138513961013`).send(`Master Bot wurde deployed & ist ready`)
     client.channels.get(`718974305660305418`).send(`Dev Bot wurde deployed & ist ready`)
 })
 
-client.user.setActivity(config.bot_settings.bot_status);
 
 // client hält logic für nachricht
 .on('message', message => {
