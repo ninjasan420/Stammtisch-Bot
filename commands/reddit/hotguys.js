ef6633896ce50f11568ddb6d6df9380c8e8994b8
Toti 犬の島 <my@cock.email>
2020-12-08 22:09:56 +0100

    Update hotguys.js

--- commands/reddit/hotguys.js
+++ commands/reddit/hotguys.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 function loadCuties(message) {
-    fetch('https://www.reddit.com/r/ratemycock.json?limit=100&?sort=top&t=day')
+    fetch('https://www.reddit.com/r/HotGuys.json?limit=100&?sort=top&t=day')
         .then(res => res.json())
         .then(json => json.data.children.map(v => v.data))
         .then(urls => postRandomCutie(urls, message))
