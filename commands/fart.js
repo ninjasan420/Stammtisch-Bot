a62df3545b6bb054e33dcf24adb335b71adf7b64
Toti 犬の島 <my@cock.email>
2021-03-07 14:56:41 +0100

    added another math solution for random image

--- commands/fart.js
+++ commands/fart.js
@@ -4,7 +4,13 @@ module.exports = {
 
     
     execute(client, message) {
-        const num = (Math.floor(Math.random() * 5) + 1).toString();
-        message.channel.send('Du Pupa! :3', { files: ["assets/images/fart${num}.gif"]})
+        const images = ["fart1", "fart2", "fart3", "fart4", "fart5", "fart6", "fart7", "fart8", "fart9" ];
+        const image = images[Math.floor(Math.random() * images.length)];
+
+        if (command === `fart`) {
+            const random = new Discord.Message
+        message.channel.send('Du Pupa! :3', random)
             }
-    };
+    }
+    
+};
