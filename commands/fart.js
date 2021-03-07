8ce34ca33d4be579ee4a270f32d1add301481cfd
Toti 犬の島 <my@cock.email>
2021-03-07 14:59:00 +0100

    defined img folder

--- commands/fart.js
+++ commands/fart.js
@@ -4,7 +4,7 @@ module.exports = {
 
     
     execute(client, message) {
-        const images = ["fart1", "fart2", "fart3", "fart4", "fart5", "fart6", "fart7", "fart8", "fart9" ];
+        const images = ["assets/images/fart1.gif", "assets/images/fart2.gif", "assets/images/fart3.gif", "assets/images/fart4.gif", "assets/images/fart5.gif", "assets/images/fart6.gif", "assets/images/fart7.gif", "assets/images/fart8.gif", "assets/images/fart9.gif" ];
         const image = images[Math.floor(Math.random() * images.length)];
 
         if (command === `fart`) {
