3c241a7a85756b60712a12ac78746c1e2bfcdf18
Toti 犬の島 <my@cock.email>
2021-03-07 13:59:50 +0100

    added fart

---
+++ commands/fart.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'fart', // command name - wird nach prefix angeführt
+    aliases: ['pups'], //
+
+    execute(client, message) {
+        message.channel.send('Du Pupa! :3 https://i.pinimg.com/originals/d8/64/c4/d864c4c6ded5a12bb0e917b1b6b5aee6.gif')
+            });
+    }
+};
