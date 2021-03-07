0477f88eb8e9c42c2153e8a38bfeac6e28869fc8
Toti 犬の島 <my@cock.email>
2021-03-07 14:46:51 +0100

    added math string to choose from random asset

--- commands/fart.js
+++ commands/fart.js
@@ -2,7 +2,9 @@ module.exports = {
     name: 'fart', // command name - wird nach prefix angeführt
     aliases: ['pups'], //
 
+    
     execute(client, message) {
-        message.channel.send('Du Pupa! :3', {files: ["assets/images/fart1.gif"]})
+        const num = (Math.floor(Math.random() * 5) + 1).toString();
+        message.channel.send('Du Pupa! :3', { files: ["assets/images/fart${num}.gif"]})
             }
     };
