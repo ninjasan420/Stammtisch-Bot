c135ad4e17265cbf2b9482992785920c04562935
Toti 犬の島 <my@cock.email>
2021-03-07 14:09:28 +0100

    fixed typo

--- commands/fart.js
+++ commands/fart.js
@@ -4,6 +4,5 @@ module.exports = {
 
     execute(client, message) {
         message.channel.send('Du Pupa! :3 https://i.pinimg.com/originals/d8/64/c4/d864c4c6ded5a12bb0e917b1b6b5aee6.gif')
-            });
-    }
-};
+            }
+    };
