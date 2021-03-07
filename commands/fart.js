91e6ffc8781cd44346d7555bd2b972e3aab3160c
Toti 犬の島 <my@cock.email>
2021-03-07 15:46:25 +0100

    Update fart.js

--- commands/fart.js
+++ commands/fart.js
@@ -1,13 +1,28 @@
+const gifs = [
+    "assets/images/fart1.gif",
+    "assets/images/fart2.gif",
+    "assets/images/fart3.gif",
+    "assets/images/fart4.gif",
+    "assets/images/fart5.gif",
+    "assets/images/fart6.gif",
+    "assets/images/fart7.gif",
+    "assets/images/fart8.gif",
+    "assets/images/fart9.gif"
+]
+
+function getRandomGif(gifs) {
+    let max = gifs.length
+    let num = (Math.floor(Math.random() * max))
+    return gifs[num]
+}
+
 module.exports = {
     name: 'fart', // command name - wird nach prefix angeführt
     aliases: ['pups'], //
 
-    
-    execute(client, message) {
-        const images = ["assets/images/fart1.gif", "assets/images/fart2.gif", "assets/images/fart3.gif", "assets/images/fart4.gif", "assets/images/fart5.gif", "assets/images/fart6.gif", "assets/images/fart7.gif", "assets/images/fart8.gif", "assets/images/fart9.gif" ];
-        const image = images[Math.floor(Math.random() * images.length)];
-        {const random = new Discord.Message
-        message.channel.send('Du Pupa! :3', random)
+
+    execute(client, message,args) {
+        message.channel.send('Du Pupa! :3', getRandomGif)
             }
     }
     
