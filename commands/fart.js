84fae2c22ea8e8783977c4ab0c9754ba75fe030a
Toti 犬の島 <my@cock.email>
2021-03-07 15:00:46 +0100

    removed if command

--- commands/fart.js
+++ commands/fart.js
@@ -6,9 +6,7 @@ module.exports = {
     execute(client, message) {
         const images = ["assets/images/fart1.gif", "assets/images/fart2.gif", "assets/images/fart3.gif", "assets/images/fart4.gif", "assets/images/fart5.gif", "assets/images/fart6.gif", "assets/images/fart7.gif", "assets/images/fart8.gif", "assets/images/fart9.gif" ];
         const image = images[Math.floor(Math.random() * images.length)];
-
-        if (command === `fart`) {
-            const random = new Discord.Message
+        {const random = new Discord.Message
         message.channel.send('Du Pupa! :3', random)
             }
     }
